a59596540f5b277eb64ef731d1017ffdaceb94f9
Rory <n/a>
2023-04-20 09:13:13 +0100

    feat: add location and time to missive

--- app/missive.py
+++ app/missive.py
@@ -1,10 +1,30 @@
+import datetime
+
+
 class Missive:
 
-    def __init__(self, name, message):
+    def __init__(self,
+                 name,
+                 message,
+                 creation_time=datetime.datetime.now()
+                 ):
         self.name = name
         self.message = message
+        self.location = self.location()
+        self.creation_time = creation_time
 
     def display_missive(self):
         print()
-        print(f"Name: {self.name}")
+        print(f"Time recorded: {self.format_creation_time()}")
+        print(f"Recording location: {self.format_location()}")
+        print(f"Scribe: {self.name}")
         print(f"Missive: {self.message}")
+
+    def format_creation_time(self):
+        return self.creation_time.strftime("%a, %d %b %Y %H:%M:%S")
+
+    def format_location(self):
+        return f"Milky Way, Sol, Earth, {self.location}"
+
+    def location(self):
+        return "British Isles, England, London"
--- test/test_missive.py
+++ test/test_missive.py
@@ -1,6 +1,16 @@
 from app.missive import Missive
+import datetime
 
 
-def test_message():
-    missive = Missive("hello")
-    assert missive.print_missive() == "Your missive is: hello"
+def test_missive_output_format(capsys):
+    missive = Missive("Rory", "Hello there", datetime.datetime(2023, 4, 20, 8, 45, 20, 56178))
+    missive.display_missive()
+    captured = capsys.readouterr()
+
+    assert captured.out == "\nTime recorded: Thu, 20 Apr 2023 08:45:20" \
+                           "\nRecording location: Milky Way, Sol, Earth, " \
+                           "British Isles, England, London" \
+                           "\nScribe: Rory" \
+                           "\nMissive: Hello there\n"
+
+
