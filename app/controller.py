78df93c1eaef194690439278ce9c405e4814fe0a
Rory <n/a>
2023-04-27 14:07:27 +0100

    ferat: introduce simple complete looping flow of program

--- app/controller.py
+++ app/controller.py
@@ -1,23 +1,45 @@
 from missive import Missive
-import random
+from missive_gateway import MissiveGateway
+from db_controller import DatabaseController
 
 
 class Controller:
 
     def __init__(self):
-        self.missives = []
+        self.missive_gateway = \
+            MissiveGateway(DatabaseController, '/Users/rory/code/missives_python/missives_staging.db')
+
+    def flow_menu(self):
+        print('Would you like to write a missive, read a missive, or learn more about this time machine?')
+        print("Type 'w' to write, 'r' to read, or 'l' to learn more, then hit enter.")
+        branch = input()
+        match branch:
+            case 'w':
+                self.get_input()
+            case 'r':
+                self.display_random_missive()
+            case 'l':
+                self.display_time_machine_info()
+            case _:
+                print("Input not recognised, please try again.")
+                print("Type ")
+        self.flow_menu()
 
     def get_input(self):
-        print("What is your name?")
+        print('What is your name?')
         name = input()
-        print("What message would you like to send to the future?")
+        print('What message would you like to send to the future?')
         message = input()
-        self.missives.append(Missive(name, message))
+        missive = Missive(name, message)
+        self.missive_gateway.insert_missive(missive)
 
     def display_random_missive(self):
-        missive = random.choice(self.missives)
+        record = self.missive_gateway.select_one_random_missive()
+        missive = Missive.init_from_record(record)
         missive.display_missive()
 
+    def display_time_machine_info(self):
+        print("Fill in time machine info")
+
 cont = Controller()
-cont.get_input()
-cont.display_random_missive()
+cont.flow_menu()
