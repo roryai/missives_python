c114b45a95001bb16d3b77cdf49acc9d0b582c51
Rory <n/a>
2023-05-10 14:56:47 +0100

    feat: OperatorMessages now printed with color and formatting

--- app/operator_message.py
+++ app/operator_message.py
@@ -1,4 +1,13 @@
 from time import sleep
+from rich import print
+
+CYAN = "#00ffff"
+MAGENTA = "#ff00ff"
+YELLOW = "#ffff00"
+PURPLE = "#af00ff"
+GREEN = "#00ff00"
+DEFAULT_COLOUR = CYAN
+HIGHLIGHT_COLOUR = MAGENTA
 
 
 class OperatorMessage:
@@ -9,46 +18,78 @@ class OperatorMessage:
         self.print_char_by_char(self.selector(command))
         print(self.pad)
 
-    def print_char_by_char(self, string):
-        for c in string:
-            print(c, end='', flush=True)
-            sleep(0.01)
+    def print_char_by_char(self, specification):
+        for x in specification:
+            message = x[0]
+            colour = x[1]
+            for char in message:
+                print(f"[bold {colour}]{char}[/bold {colour}]", end='', flush=True)
+                sleep(0.003)
 
     def selector(self, command):
         match command:
             case 'read_write_learn':
-                return "Would you like to write a missive, read a missive, or learn more about this time machine?\n" \
-                 "Type 'w' to write, 'r' to read, or 'l' to learn more, then hit enter."
+                return self.read_write_learn()
 
             case 'what_is_your_name':
-                return 'What is your name?'
+                return [('What is your name?', DEFAULT_COLOUR)]
 
             case 'what_is_your_message':
-                return 'What message would you like to send to the future?'
+                return [('What message would you like to send to the future?', DEFAULT_COLOUR)]
 
             case 'input_not_recognised':
-                return 'Input not recognised, please try again.'
+                return [('⚠️ Input not recognised, please try again. ⚠️', YELLOW)]
 
             case 'name_only_letters':
-                return 'Name must consist of only letters.'
+                return [('⚠️ Name must consist of only letters. ⚠️', YELLOW)]
 
             case 'message_too_short':
-                return 'Your message is too short to be recorded. Please enter another message.'
+                return [('⚠️ Your message is too short to be recorded. Please enter another message. ⚠️', YELLOW)]
 
             case 'missive_recorded':
-                return 'Your missive has been added to the temporal archive, thank you.'
+                return [('Your missive has been added to the temporal archive, thank you.', MAGENTA)]
 
             case 'enter_to_continue':
-                return 'Hit enter to continue.'
+                return self.hit_enter_to_continue()
 
             case 'display_another_missive_or_continue':
-                return "Enter 'r' to read another missive, or hit enter to continue"
+                return self.display_another_missive_or_continue()
 
             case 'time_machine_info':
                 return self.time_machine_info()
 
+    def read_write_learn(self):
+        return [
+            ("Would you like to write a missive, read a missive, or learn more about this time machine?", GREEN),
+            ("\nType",                          DEFAULT_COLOUR),
+            (" w",                              HIGHLIGHT_COLOUR),
+            (" to write,",                      DEFAULT_COLOUR),
+            (" r",                              HIGHLIGHT_COLOUR),
+            (" to read, or",                    DEFAULT_COLOUR),
+            (" l",                              HIGHLIGHT_COLOUR),
+            (" to learn more, then hit",        DEFAULT_COLOUR),
+            (" enter",                          HIGHLIGHT_COLOUR),
+            (".",                               DEFAULT_COLOUR),
+        ]
+
+    def hit_enter_to_continue(self):
+        return [
+            ('Hit',             DEFAULT_COLOUR),
+            (' enter',          HIGHLIGHT_COLOUR),
+            (' to continue.',   DEFAULT_COLOUR)
+        ]
+
+    def display_another_missive_or_continue(self):
+        return [
+            ("Enter",                                   DEFAULT_COLOUR),
+            (" r",                                      HIGHLIGHT_COLOUR),
+            (" to read another missive, or hit",        DEFAULT_COLOUR),
+            (" enter",                                  HIGHLIGHT_COLOUR),
+            (" to continue.",                           DEFAULT_COLOUR)
+        ]
+
     def time_machine_info(self):
-        return """
+        return [("""
     This is a time machine that allows you to send messages to people in the future.
     
     It was discovered aboard the wreck of the Mary Celeste, in a centuries-old air pocket.
@@ -68,4 +109,4 @@ class OperatorMessage:
     of the creators.
     
     We do not know who else may be reading your messages.
-        """
+        """, PURPLE)]
