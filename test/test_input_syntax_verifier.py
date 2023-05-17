584adf67cc8069d07e706678995fb3ff8ac16e47
Rory <n/a>
2023-05-17 16:24:24 +0100

    test: separate out tests to isolate behaviour, improve test names

--- test/test_input_syntax_verifier.py
+++ test/test_input_syntax_verifier.py
@@ -3,25 +3,35 @@ from app.input_syntax_verifier import InputSyntaxVerifier
 verifier = InputSyntaxVerifier()
 
 
-def test_check_name_true():
+def test_name_must_begin_with_two_letters():
     names = [
         "AB",
         "Ab 2",
         "AB $3.50",
-        "AB $3.50  ",
+        "AB $3.50  "
+    ]
+    for name in names:
+        assert verifier.check_name(name) is True
+
+
+def test_name_can_be_up_to_30_chars_long():
+    names = [
         "This is exactly 30 chars, see!",
     ]
     for name in names:
         assert verifier.check_name(name) is True
 
 
-def test_check_name_fails():
+def test_names_cannot_have_spaces_numbers_or_special_chars_in_first_two_chars():
     names = [
         "A B",
         "2AB",
         "!AB",
-        " Ab",
-        "This is one char over 30 limit!",
+        " Ab"
     ]
     for name in names:
         assert verifier.check_name(name) is not True
+
+def test_names_cannot_be_over_30_chars():
+    name = "This is one char over 30 limit!"
+    assert verifier.check_name(name) is False
