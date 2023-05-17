66c6c23b6211ff3828fc2d6004bdc6e085b1cd9f
Rory <n/a>
2023-05-17 17:10:27 +0100

    feat: add message length checks

--- app/input_syntax_verifier.py
+++ app/input_syntax_verifier.py
@@ -1,6 +1,17 @@
 import re
 
+
 class InputSyntaxVerifier:
 
+    # name must begin with two letters and be up to 30 chars in length
     def check_name(self, name):
-        return re.search("^[a-zA-Z]{2,}", name) and len(name) <= 30
+        return bool(re.search("^[a-zA-Z]{2,}", name)) and len(name) <= 30
+
+    # message must be minimum 10 chars in length excluding spaces,
+    def check_message_min_length(self, message):
+        message = message.replace(" ", "")
+        return bool(re.search(r"\S{10,}", message))
+
+    # maximum message length is 1000 chars including spaces
+    def check_message_max_length(self, message):
+        return len(message) <= 1000
--- test/test_input_syntax_verifier.py
+++ test/test_input_syntax_verifier.py
@@ -30,8 +30,94 @@ def test_names_cannot_have_spaces_numbers_or_special_chars_in_first_two_chars():
         " Ab"
     ]
     for name in names:
-        assert verifier.check_name(name) is not True
+        assert verifier.check_name(name) is False
+
 
 def test_names_cannot_be_over_30_chars():
     name = "This is one char over 30 limit!"
     assert verifier.check_name(name) is False
+
+
+def test_message_is_verified_when_it_contains_at_least_10_chars():
+    messages = [
+        "1234567890",
+        "123 4567890",
+        "abcdefghij",
+        "abcde   fghij",
+        "abcde12345",
+        "!@£$%^&*()",
+        "abc123&*()",
+        "abc1   23&*()",
+    ]
+    for message in messages:
+        assert verifier.check_message_min_length(message) is True
+
+
+def test_message_is_not_verified_when_it_contains_less_than_10_chars():
+    messages = [
+        "123456789",
+        "123456 789",
+        "abcdefghi",
+        "abcdefg hi",
+        "          ",
+        "    123456789      ",
+    ]
+    for message in messages:
+        assert verifier.check_message_min_length(message) is False
+
+
+def test_message_is_verified_if_exactly_1000_chars_in_length():
+    message_of_1000_chars = """
+    This is a time machine that allows you to send messages to people in the future.
+    
+    It was discovered aboard the wreck of the Mary Celeste, in the captain's cabin.
+    
+    The machine was operational and running at the time of discovery.
+    
+    It is made of exotic materials, some of which were previously unknown to science.
+    
+    Messages can be recorded by many means, including using a pen or stylus.
+    A keyboard has been deemed to be the most desirable interface for 21st century 
+    users due to its familiarity.
+    
+    The machine does not record the electrical output from the keyboard. Instead, it
+    records the intent of the user as expressed through the choices they make about
+    which keys to press. When a robot presses the keys nothing happens.
+    
+    The mechanism by which this machine operates is unknown, as is the origin or 
+    identity of the creators.
+    
+    We do not know who else may be reading your messages.
+    
+    a few more then
+        """
+    assert verifier.check_message_max_length(message_of_1000_chars) is True
+
+
+def test_message_is_not_verified_if_over_1000_chars_in_length():
+    message_of_1001_chars = """
+    This is a time machine that allows you to send messages to people in the future.
+    
+    It was discovered aboard the wreck of the Mary Celeste, in the captain's cabin.
+    
+    The machine was operational and running at the time of discovery.
+    
+    It is made of exotic materials, some of which were previously unknown to science.
+    
+    Messages can be recorded by many means, including using a pen or stylus.
+    A keyboard has been deemed to be the most desirable interface for 21st century 
+    users due to its familiarity.
+    
+    The machine does not record the electrical output from the keyboard. Instead, it
+    records the intent of the user as expressed through the choices they make about
+    which keys to press. When a robot presses the keys nothing happens.
+    
+    The mechanism by which this machine operates is unknown, as is the origin or 
+    identity of the creators.
+    
+    We do not know who else may be reading your messages.
+    
+    a few more then!
+        """
+    assert verifier.check_message_max_length(message_of_1001_chars) is False
+
