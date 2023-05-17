b34e32216a54f2f9f8651bc132d6fc39e2c9559b
Rory <n/a>
2023-05-17 10:39:45 +0100

    feat: add a file that outputs all varieties of text output
    to enable a rapid feedback loop when experimenting with different colour palettes

---
+++ resources/test_card.py
@@ -0,0 +1,23 @@
+from missives_python.app.missive import Missive
+from missives_python.app.printer import Printer
+from missives_python.app.operator_message import OperatorMessage
+import missives_python.app.styles as styles
+from datetime import datetime
+
+
+printer = Printer(True)
+
+info_message = 'This is a test message to display the text formatting.' \
+               '\nI am using this message as the real message is too long and will not ' \
+               '\nfit on the same screen as everything else.' \
+
+missive = Missive(name="Rory", message=info_message,
+                  creation_time=datetime(2023, 4, 20, 8, 45, 20, 56178), printer=printer)
+
+OperatorMessage('read_write_learn', printer)
+OperatorMessage('name_only_letters', printer)
+OperatorMessage('missive_recorded', printer)
+OperatorMessage('what_is_your_name', printer)
+printer.process([(info_message, styles.INFO)])
+missive.display_missive()
+OperatorMessage('enter_to_continue', printer)
